bad3ebc25d25007c04a893859918e1c844731a81
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2023-04-20 19:17:46 -0300

    punto4.c

--- punto4.c
+++ punto4.c
@@ -3,20 +3,20 @@
 #include<string.h>
 
 
-struct Producto {
+typedef struct Producto {
 int ProductoID; //Numerado en ciclo iterativo
 int Cantidad; // entre 1 y 10
 char *TipoProducto; // Algún valor del arreglo TiposProductos
 float PrecioUnitario; // entre 10 - 100
-}typedef Producto;
+}Producto;
 
-struct Cliente {
+typedef struct Cliente {
 int ClienteID; // Numerado en el ciclo iterativo
 char *NombreCliente; // Ingresado por usuario
 int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
 Producto *Productos; //El tamaño de este arreglo depende de la variable
 // “CantidadProductosAPedir”
-}typedef Cliente;
+}Cliente;
 
 int main(){
     char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
@@ -24,10 +24,8 @@ int main(){
     printf("Ingrese la cantidad de clientes:\n");
     scanf("%d",&cantClientes);
 
+    Cliente* datos;
+    datos=CargarClientes
     
-
-
-
-
     return 0;
 }
