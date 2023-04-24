92d83a8e10e49d409772480f967e5ad674deea1c
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2023-04-24 20:30:39 -0300

    punto 5 - preguntar

---
+++ p5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i,cant;
+    char** nombres;
+    char* buff;
+    buff= (char *) malloc(200*sizeof(char));
+    printf("Ingrese la cant de nombres:\n");
+    scanf("%d",&cant);
+    printf("Ingresar los nombres:\n"),
+    nombres=(char**) malloc(cant*sizeof(char));
+    fflush(stdin);
+    for(i=0;i<cant;i++){
+    	nombres[i]=(char*) malloc((strlen(buff)+1) * sizeof(char));
+    	
+        printf("%d:",i+1);
+        fflush(stdin);
+        gets(buff);
+        strcpy(nombres[i],buff);
+    
+    }
+
+    printf("NOMBRES INGRESADOS:\n");
+    for(i=0;i<cant;i++){
+
+        printf("%s\n",nombres[i]);
+
+
+    }
+
+
+    for(i=0;i<cant;i++){
+        free(nombres[i]);
+
+    }
+
+
+    free(nombres);
+    return 0;
+}
